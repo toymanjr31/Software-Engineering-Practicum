af281316f77a9129efcc84d4c0cf79625ad98a37
toymanjr31 <toymanjr31@gmail.com>
2020-11-08 22:02:35 +0700

    Fill PastPaper Class

--- PastPaperBank/src/pastpaperbank/PastPaper.java
+++ PastPaperBank/src/pastpaperbank/PastPaper.java
@@ -1,14 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pastpaperbank;
 
-/**
- *
- * @author User
- */
 public class PastPaper {
+    public byte subjectid;
+    public String year;
+    public String lecturer;
     
+    public String subjectName(){
+        String subject = null;
+        switch(subjectid){
+            case 1:{
+                subject = "Programming 1";
+                break;
+            }
+            case 2:{
+                subject = "Programming 2";
+                break;
+            }
+            case 3:{
+                subject = "Database";
+                break;
+            }
+            case 4:{
+                subject = "Software Engineering";
+                break;
+            }
+        }
+        return subject;
+    }
+    public static void main(String[] args){
+        PastPaper SE1 = new PastPaper();
+        SE1.lecturer = "Isna Asfi Mudzolifah";
+        SE1.year = "2019";
+        SE1.subjectid = 4;
+        System.out.println(SE1.subjectName());
+    }
 }
